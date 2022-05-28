a00985d2e1504504c83b8364fb9ac381d0f2164b
Varol Aksoy <varolaksoy@gmail.com>
2022-05-28 13:06:55 +0300

    add TopRatedMovies response model

---
+++ TheMovieDB/Modules/Home/Entity/TopRatedMovies.swift
@@ -0,0 +1,50 @@
+//
+//  TopRatedMovies.swift
+//  TheMovieDB
+//
+//  Created by Varol Aksoy on 28.05.2022.
+//
+
+import Foundation
+
+// MARK: - TopRatedMoviesResponse
+struct TopRatedMoviesResponse: Codable {
+    let page: Int?
+    let results: [Movie]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Movie
+struct Movie: Codable {
+    let backdropPath: String?
+    let firstAirDate: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let name: String?
+    let originCountry: [String]?
+    let originalLanguage, originalName, overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case backdropPath = "backdrop_path"
+        case firstAirDate = "first_air_date"
+        case genreIDS = "genre_ids"
+        case id, name
+        case originCountry = "origin_country"
+        case originalLanguage = "original_language"
+        case originalName = "original_name"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
